0de4770a980983e78837eebe28b8865e5d318d63
hemaoling3 <hemaoling3@xdf.cn>
2021-07-22 13:54:33 +0800

    leetcode

---
+++ test-demo/src/main/java/com/myself/test/TestJDK.java
@@ -0,0 +1,21 @@
+package com.myself.test;
+
+import java.util.AbstractList;
+
+public class TestJDK {
+
+    public static void main(String[] args) {
+        ClassLoader classLoader;
+
+        Compiler compiler;
+
+        System system;
+
+        System.err.println(2345345);
+
+
+        System.out.println(System.getenv("123"));
+
+        AbstractList abstractList;
+    }
+}
