d4c638ddac4a6923eebf36f373d5769aebfb9c8e
hemaoling3 <hemaoling3@xdf.cn>
2021-08-02 10:58:01 +0800

    leetcode

--- test-demo/src/main/java/com/myself/jvmtest/CaculateTestDemo.java
+++ test-demo/src/main/java/com/myself/jvmtest/CaculateTestDemo.java
@@ -1,4 +1,10 @@
 package com.myself.jvmtest;
 
 public class CaculateTestDemo {
+    public int demo() {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        return (a + b) * c;
+    }
 }
