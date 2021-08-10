6939cbe71cf1b8f7167fec7b1db159aa75df75cb
hemaoling3 <hemaoling3@xdf.cn>
2021-08-10 23:08:00 +0800

    leetcode

--- test-demo/src/main/java/com/myself/test/TestJDK.java
+++ test-demo/src/main/java/com/myself/test/TestJDK.java
@@ -2,10 +2,8 @@ package com.myself.test;
 
 import org.apache.tomcat.jni.Thread;
 
-import java.util.AbstractList;
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.*;
+import java.util.concurrent.*;
 import java.util.concurrent.locks.AbstractQueuedSynchronizer;
 import java.util.concurrent.locks.Condition;
 
@@ -18,11 +16,6 @@ public class TestJDK {
 
         System system;
 
-        System.err.println(2345345);
-
-
-        System.out.println(System.getenv("123"));
-
         AbstractList abstractList;
 
         ThreadPoolExecutor threadPoolExecutor;
@@ -33,6 +26,53 @@ public class TestJDK {
 
         LinkedBlockingQueue linkedBlockingQueue;
         LinkedBlockingDeque linkedBlockingDeque;
+
+
+        ArrayList arrayList;
+
+        AbstractMap abstractMap;
+
+        AbstractSet abstractSet;
+
+        HashMap hashMap;
+
+        Hashtable hashtable;
+
+
+        HashSet hashSet;
+
+        LinkedHashMap<Integer, Integer> linkedHashMap = new LinkedHashMap<>();
+        testLinkedHashMap(linkedHashMap);
+
+
+        LinkedHashSet linkedHashSet;
+
+        TreeMap treeMap;
+
+        TreeSet treeSet;
+
+        Queue queue;
+
+        ConcurrentHashMap concurrentHashMap;
+
+        CompletableFuture completableFuture;
+
+        LinkedList linkedList;
+
+
+    }
+
+    private static void testLinkedHashMap(LinkedHashMap<Integer, Integer> linkedHashMap) {
+        linkedHashMap.put(1, 1);
+        linkedHashMap.put(2, 2);
+        linkedHashMap.put(3, 3);
+        linkedHashMap.put(13, 3);
+        linkedHashMap.put(12, 3);
+        linkedHashMap.put(14, 3);
+        System.out.println(linkedHashMap);
+        linkedHashMap.get(2);
+        System.out.println(linkedHashMap);
+
 
     }
 }
