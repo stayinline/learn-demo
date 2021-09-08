0b78810bb3a9e7dbbdd7b65151f63c801a13980a
hemaoling <hemaoling3@xdf.cn>
2021-09-08 20:29:48 +0800

    CompletionService

--- concurrent-learn/src/main/Java/concurrency_in_pritace/demo2/CompletionServiceTest.java
+++ concurrent-learn/src/main/Java/concurrency_in_pritace/demo2/CompletionServiceTest.java
@@ -1,12 +1,38 @@
 package concurrency_in_pritace.demo2;
 
-import java.util.concurrent.CompletionService;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorCompletionService;
+import concurrency_in_pritace.demo1.TestData;
+
+import java.util.concurrent.*;
 
 public class CompletionServiceTest {
+    private static ExecutorService service = new ThreadPoolExecutor(2, 2, 1000, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
+
+    /**
+     * 一组任务一起执行，并结合BlockingQueue的特性，获取结果
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-        Executor executor;
-        //CompletionService completionService = new ExecutorCompletionService<Void>(executor);
+        CompletionService<TestData> completionService = new ExecutorCompletionService<>(service);
+        int count = 10;
+        for (int i = 0; i < count; i++) {
+            completionService.submit(() -> {
+                System.out.println("处理业务");
+                return new TestData();
+            });
+        }
+
+        for (int i = 0; i < count; i++) {
+            try {
+                Future<TestData> future = completionService.take();
+                Future<TestData> future2 = completionService.poll(3000, TimeUnit.MILLISECONDS);
+                TestData testData = future.get();
+                System.out.println("处理结果: " + testData.toString());
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 }
